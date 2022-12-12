910b88dc5c12fe7a540eebdb6b03b49c59c50b3b
Kaleab-Ayenew <kaleab.ayenew2013@gmail.com>
2022-12-12 14:59:22 +0300

    regular commit24

--- main/note/serializers.py
+++ main/note/serializers.py
@@ -3,17 +3,13 @@ from note.models import Note
 from django.contrib.auth.models import User
 
 class NoteSerializer(serializers.HyperlinkedModelSerializer):
-
     owner = serializers.ReadOnlyField(source="owner.username")
-
     class Meta:
         model = Note
         fields = '__all__'
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
-
     notes = serializers.HyperlinkedRelatedField(many=True, view_name='note-content', read_only=True)
-
     class Meta:
         fields = "__all__"
         model = User
