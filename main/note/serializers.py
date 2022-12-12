111dcc39ce4da67f8c04337b1c52cef3f7bb9c33
Kaleab-Ayenew <kaleab.ayenew2013@gmail.com>
2022-12-12 14:48:46 +0300

    regular commit14

--- main/note/serializers.py
+++ main/note/serializers.py
@@ -4,3 +4,7 @@ from note.models import Note
 class NoteSerializer(serializers.ModelSerializer):
 
     owner = serializers.ReadOnlyField(source="owner.username")
+
+    class Meta:
+        model = Note
+        fields = '__all__'
