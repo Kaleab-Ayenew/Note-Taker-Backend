85413d91a6142e43b329b19e2908c597ef7fce1c
Kaleab-Ayenew <kaleab.ayenew2013@gmail.com>
2022-12-12 14:39:13 +0300

    regular commit5

--- main/note/models.py
+++ main/note/models.py
@@ -1,3 +1,8 @@
 from django.db import models
-
+from django.contrib.models.auth import User
 # Create your models here.
+class Note(models.Model):
+    owner = User
+    title = models.CharField(max_length=250)
+    content = models.TextField()
+
