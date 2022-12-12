e50a2e5672a9b818719bf468854ecf20ad8590ba
Kaleab-Ayenew <kaleab.ayenew2013@gmail.com>
2022-12-12 14:41:20 +0300

    regular commit7

--- main/note/models.py
+++ main/note/models.py
@@ -6,4 +6,5 @@ class Note(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=250)
     content = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
 
