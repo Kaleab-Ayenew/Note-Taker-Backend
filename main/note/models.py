ba43d6cfda228f7e0798ad464aa732ea41213a5b
Kaleab-Ayenew <kaleab.ayenew2013@gmail.com>
2022-12-12 14:40:16 +0300

    regular commit6

--- main/note/models.py
+++ main/note/models.py
@@ -1,8 +1,9 @@
 from django.db import models
-from django.contrib.models.auth import User
+from django.contrib.auth.models import User
 # Create your models here.
+
 class Note(models.Model):
-    owner = User
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=250)
     content = models.TextField()
 
