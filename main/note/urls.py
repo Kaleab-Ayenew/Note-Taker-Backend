bb57ef4c951be17f054c0cf24007e3f67ef307ce
Kaleab-Ayenew <kaleab.ayenew2013@gmail.com>
2022-12-12 15:51:15 +0300

    regular commit67

--- main/note/urls.py
+++ main/note/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from note import views
 urlpatterns = [
-    path('all-notes/', ),
-    path('note-content', include(''))
+    path('all-notes/', views.note_list, 'note_list_url'),
+    path('note-content', views.note_content, 'note_content_url')
 ]
