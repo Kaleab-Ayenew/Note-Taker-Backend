abfe3b703b0323eac2b4634381aaebf06a6d7c80
Kaleab-Ayenew <kaleab.ayenew2013@gmail.com>
2022-12-12 15:13:09 +0300

    regular commit37

--- main/note/views.py
+++ main/note/views.py
@@ -1,11 +1,13 @@
 from note.models import Note
 from django.contrib.auth.models import User
-
+from django.forms import 
 #DRF Imports
 
 from rest_framework.decorators import api_view
 from rest_framework.urlpatterns import format_suffix_patterns
+from rest_framework.response import Response
 
 api_view['GET','POST']
 def note_list(request, format=None):
 
+    if request.method ==
