7474ca731cfcfc6667cdd150129c86adeef5b558
Kaleab-Ayenew <kaleab.ayenew2013@gmail.com>
2022-12-12 15:11:02 +0300

    regular commit35

--- main/note/views.py
+++ main/note/views.py
@@ -1,5 +1,11 @@
 from note.models import Note
 from django.contrib.auth.models import User
 
+#DRF Imports
+
 from rest_framework.decorators import api_view
-# Create your views here.
+from rest_framework.urlpatterns import format_suffix_patterns
+
+api_view['GET','POST']
+def note_list(request, format=None):
+
