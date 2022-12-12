8537870816191b425075fe7747850fc5fdcf4e7f
Kaleab-Ayenew <kaleab.ayenew2013@gmail.com>
2022-12-12 15:14:13 +0300

    regular commit38

--- main/note/views.py
+++ main/note/views.py
@@ -1,13 +1,19 @@
 from note.models import Note
 from django.contrib.auth.models import User
-from django.forms import 
+
 #DRF Imports
 
 from rest_framework.decorators import api_view
 from rest_framework.urlpatterns import format_suffix_patterns
 from rest_framework.response import Response
 
+from note.serializers import NoteSerializer, UserSerializer
+
 api_view['GET','POST']
 def note_list(request, format=None):
 
-    if request.method ==
+    if request.method == 'GET':
+
+        notes = Note.objects.all()
+        serializer = NoteSerializer(notes)
+
