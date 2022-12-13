625317fbc3ea70c74fe387586573724b820379eb
Kaleab-Ayenew <kaleab.ayenew2013@gmail.com>
2022-12-13 13:35:54 +0300

    regular commit333

--- main/accounts/urls.py
+++ main/accounts/urls.py
@@ -3,7 +3,7 @@ from accounts import views
 
 urlpatterns = [
     path("sign-up/", views.signup_view),
-    path("login", views.login_view),
+    path("login/", views.login_view),
     path("logout/", views.logout_view),
     path("edit-user/<str:username>/", views.user_actions)
 ]
