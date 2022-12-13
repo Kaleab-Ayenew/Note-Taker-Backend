5e4d26fb30dbe8d011cdac6c16e4f09edd4f44cf
Kaleab-Ayenew <kaleab.ayenew2013@gmail.com>
2022-12-13 11:30:40 +0300

    regular commit222

--- main/accounts/views.py
+++ main/accounts/views.py
@@ -1,5 +1,4 @@
 #DRF Imports
-
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
@@ -8,3 +7,4 @@ from rest_framework.response import Response
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
 from django.shortcuts import redirect
+
