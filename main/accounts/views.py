40d0903054bf8e7426d3a3322936c464d3b5493d
Kaleab-Ayenew <kaleab.ayenew2013@gmail.com>
2022-12-13 11:53:08 +0300

    regular commit242

--- main/accounts/views.py
+++ main/accounts/views.py
@@ -1,17 +1,25 @@
 #DRF Imports
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-
+from rest_framework import status
 
 #DJANGO IMPORTS
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
 from django.shortcuts import redirect
 
+#Application imports
+from accounts.serializers import UserSerializer
+
 api_view(['POST'])
 def sign_up(request):
 
     user_data = request.data
-    new_user_name = user_data['username']
-    new_password = user_data['password']
+    serializer = UserSerializer(user_data)
+
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.validated_data, status=status.HTTP_201_CREATED)
     
+
+
