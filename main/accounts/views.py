7c9754439fccf7bbd04838a33c05eb88a826f984
Kaleab-Ayenew <kaleab.ayenew2013@gmail.com>
2022-12-13 14:16:05 +0300

    regular commit368

--- main/accounts/views.py
+++ main/accounts/views.py
@@ -43,14 +43,14 @@ def login_view(request):
         login_data = {"status":"failed"}
         return Response(login_data, status=status.HTTP_401_UNAUTHORIZED)
 
-@login_required
+
 @api_view(['POST'])
 def logout_view(request):
     logout(request)
     logout_status = {"status":"ok"}
     return Response(logout_status, status=status.HTTP_200_OK)
 
-@login_required(login_url=reverse("login_url"))
+@login_required(login_url=reverse("my_login_url")) 
 @api_view(['GET','PUT', 'DELETE'])
 def user_actions(request, username):
     user_instance = get_object_or_404(User, username=username)
