818f77e2cfd0f1b756c01e1a0f9df9a021e1f4c2
Kaleab-Ayenew <kaleab.ayenew2013@gmail.com>
2022-12-13 11:28:33 +0300

    regular commit220

--- main/accounts/views.py
+++ main/accounts/views.py
@@ -1,3 +1,4 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+from django.shortcuts import redirect
