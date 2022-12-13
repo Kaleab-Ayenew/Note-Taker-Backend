9d9c4d7351166dfdcd5a056dcc9ee49299c2f492
Kaleab-Ayenew <kaleab.ayenew2013@gmail.com>
2022-12-13 11:34:09 +0300

    regular commit225

--- main/accounts/views.py
+++ main/accounts/views.py
@@ -8,3 +8,10 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
 from django.shortcuts import redirect
 
+api_view(['POST'])
+def sign_up(request):
+
+    user_data = request.data
+    new_user_name = user_data['username']
+    new_password = user_data['password']
+    
