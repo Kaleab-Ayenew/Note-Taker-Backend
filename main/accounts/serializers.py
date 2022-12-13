79418d1fca7ab9be3274c5f4a569cb572575e030
Kaleab-Ayenew <kaleab.ayenew2013@gmail.com>
2022-12-13 11:37:20 +0300

    regular commit228

--- main/accounts/serializers.py
+++ main/accounts/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
-
+from note.models import Note
 
 class UserSerializer(serializers.ModelSerializer):
     notes = serializers.RelatedField(many=True, queryset= Note.objects.all())
