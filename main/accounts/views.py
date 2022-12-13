f15480472b76c8de8522ee95aff9ce8c8f916f2a
Kaleab-Ayenew <kaleab.ayenew2013@gmail.com>
2022-12-13 12:02:05 +0300

    regular commit249

--- main/accounts/views.py
+++ main/accounts/views.py
@@ -12,16 +12,30 @@ from django.shortcuts import redirect
 from accounts.serializers import UserSerializer
 
 @api_view(['POST'])
-def sign_up(request):
+def signup_view(request):
 
     user_data = request.data
-    serializer = UserSerializer(user_data)
+    serializer = UserSerializer(data=user_data)
 
     if serializer.is_valid():
         serializer.save()
         return Response(serializer.validated_data, status=status.HTTP_201_CREATED)
     else:
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+@api_view
+def login_view(request):
+
+    login_data = request.data
+    serializer = UserSerializer(data=login_data)
+
+    if serializer.is_valid():
+        user_name = serializer.data['username']
+        password = serializer.data['password']
+
+        user_instance = authenticate(request, username=user_name, password=password)
+        if user_instance is not None:
+
+            login(request, user_instance)
 
 
     
