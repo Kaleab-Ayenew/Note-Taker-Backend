e8713b8b1e49b783e521a92dcb7bed0cff501b06
Kaleab-Ayenew <kaleab.ayenew2013@gmail.com>
2022-12-13 12:34:44 +0300

    regular commit278

--- main/accounts/urls.py
+++ main/accounts/urls.py
@@ -2,5 +2,8 @@ from django.urls import path, include
 from accounts import views
 
 urlpatterns = [
-    
+    path("sign-up/", views.signup_view),
+    path("login", views.login_view),
+    path("logout/", views.logout_view),
+    path("edit-user/", views.user_actions)
 ]
