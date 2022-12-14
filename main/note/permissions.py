2b9e4e06297c62372fa67bcc65fc98a59d7221f7
Kaleab-Ayenew <kaleab.ayenew2013@gmail.com>
2022-12-14 14:18:53 +0300

    regular commit446

--- main/note/permissions.py
+++ main/note/permissions.py
@@ -2,6 +2,8 @@ from rest_framework.permissions import BasePermission
 
 class OwnsThisObject(BasePermission):
 
+    message = "You don't have permission to acess this data."
+
     def has_object_permission(self, request, view, obj):
         if request.user.is_superuser:
             return True
