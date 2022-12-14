0a125f741df37e5b8ece95ae8ed1ee2fc326dd5b
Kaleab-Ayenew <kaleab.ayenew2013@gmail.com>
2022-12-14 14:16:46 +0300

    regular commit444

--- main/note/permissions.py
+++ main/note/permissions.py
@@ -3,8 +3,9 @@ from rest_framework.permissions import BasePermission
 class OwnsThisObject(BasePermission):
 
     def has_object_permission(self, request, view, obj):
-        
-        if request.user == obj.owner:
+        if request.user.is_superuser:
+            return True
+        elif request.user == obj.owner:
             return True
         else:
             return False
